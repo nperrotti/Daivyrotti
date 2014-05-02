e28c994787f5f47ce4c048a081e24ae566e1b0ff
Ivy Wong <ivy.wong022@gmail.com>
2014-05-01 23:30:49 -0400

    Update KingCrab

--- GridWorldCode/projects/critters/KingCrab.java
+++ GridWorldCode/projects/critters/KingCrab.java
@@ -6,31 +6,30 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class KingCrab extends CrabCritter {
+
     public KingCrab(){
         setColor(Color.RED);
     }
-    
 
     public void processActors(ArrayList<Actor> actors) {
-	loc = this.getLocation();
-	for (Actor a : actors){
-	    boolean remove = true;
-	    ArrayList<Location> locs = getGrid().getEmptyAdjacentLocations(a.getLocation());
-	    for(Location l:locs){
-		
-		if ( (a.getLocation.getRow()> loc && l.getRow() > loc + 1) ||
-		     (a.getLocation.getColumn()> loc && l.getColumn() > loc + 1) ||
-		     (a.getLocation.getColumn()< loc && l.getColumn() < loc - 1) ||
-		     (a.getLocation.getRow()< loc && l.getRow() < loc - 1) )
-		    {
-      			a.moveTo(l);
-			remove = false;
-		    }
-		     
-		if (remove) {
-		    a.removeSelfFromGrid();
-		}
-	    }
-	}
+        Location loc = getLocation();
+        int row = loc.getRow();
+        int col = loc.getCol();
+        for (Actor a : actors){
+            boolean remove = true;
+            ArrayList<Location> locs = getGrid().getEmptyAdjacentLocations(a.getLocation());
+            for(Location l:locs){
+                if ( (a.getLocation().getRow() > row && l.getRow() > row + 1) ||
+                        (a.getLocation().getCol() > col && l.getCol() > col + 1) ||
+                        (a.getLocation().getCol() < col && l.getCol() < col - 1) ||
+                        (a.getLocation().getRow() < row && l.getRow() < row - 1) ) {
+                    a.moveTo(l);
+                    remove = false;
+                }
+                if (remove) {
+                    a.removeSelfFromGrid();
+                }
+            }
+        }
     }
 }
